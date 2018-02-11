9d5c30924815c921eaa47c2fc16bdae24f6dc8e8
m3ntalhtml <36369043+m3ntalhtml@users.noreply.github.com>
2018-02-11 08:11:13 -0600

    Update Master.js

--- Master.js
+++ Master.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-const prefix = "ez-"
+const prefix = "e!"
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
