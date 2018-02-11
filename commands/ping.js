698ef4a52cf11ada8ff9629662547aef9aba0acd
m3ntalhtml <36369043+m3ntalhtml@users.noreply.github.com>
2018-02-11 07:45:10 -0600

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {
+const embed = new Discord.RichEmbed()
+  .setTitle("Ping")
+  .setAuthor(message.author.username, message.author.avatarURL)
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("Ping? Pong! Your response time was " + client.ping + "milliseconds!")
+  .setFooter("Evisocrat", message.author.defaultAvatarURL)
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  /*
+  * Wew
+  */
+    message.channel.send({embed}).catch(console.error);
+}
