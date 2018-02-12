dec1c0e9c2e5b477300528bedfb3eb3d5ebec5e3
m3ntalhtml <36369043+m3ntalhtml@users.noreply.github.com>
2018-02-12 16:09:43 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ const embed = new Discord.RichEmbed()
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
-  .setDescription("Ping? Pong! Your response time was " + client.ping + "milliseconds!")
+  .setDescription("Ping? Pong! Your response time was " + client.ping + " milliseconds!")
   .setFooter("Evisocrat", message.author.defaultAvatarURL)
   /*
    * Takes a Date object, defaults to current date.
